3072a96b366faaf93b259be0c1186caebf481e54
Shasuna Lin <162057763+shasunal@users.noreply.github.com>
2025-05-06 11:38:07 -0400

    homework

---
+++ config/db.js
@@ -0,0 +1,28 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://linshasuna:homework12@homework12.exffwdc.mongodb.net/?retryWrites=true&w=majority&appName=Homework12";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+  tlsAllowInvalidCertificates: true 
+});
+
+const databaseConnection = {
+  client: client,
+  run: async function () {
+    try {
+      const db = client.db('petsData');
+      const pets = db.collection('petsCollection');
+      const query = { type: 'Cat' }; // test query for demo
+      const result = await pets.findOne(query);
+      console.log('Test query result:', result);
+    } finally {
+      await client.close();
+    }
+  }
+};
+
+module.exports = databaseConnection;
